ec33bdbc40c8cdfebda6548351caef9c02fc8001
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2021-05-01 16:15:07 +0200

    Improve log messages

--- Opal/About/private/worker_spdx.js
+++ Opal/About/private/worker_spdx.js
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+var LOG_SCOPE = '[Opal.About]'
+
 function sendError(spdxId) {
     WorkerScript.sendMessage({
         spdxId: spdxId,
@@ -49,19 +51,19 @@ function loadRemote(spdxId, localUrl, remoteUrl) {
         try {
             var o = JSON.parse(xhr.responseText);
             if (!o || typeof o !== "object") throw 1;
-            console.log("license loaded remotely from", remoteUrl);
+            console.log(LOG_SCOPE, "license loaded remotely from", remoteUrl);
             sendSuccess(spdxId, o['name'], o['licenseText']);
 
             request("PUT", localUrl, function(x){
-                console.log("saved license with status", x.status, "to", localUrl);
+                console.log(LOG_SCOPE, "saved license with status", x.status, "to", localUrl);
             }, function(x){}, xhr.responseText);
         }
         catch (e) {
-            console.log("failed to load license remotely from", remoteUrl);
+            console.log(LOG_SCOPE, "failed to load license remotely from", remoteUrl);
             sendError(spdxId);
         }
     }, function(xhr) {
-        console.log("failed to load license remotely from", remoteUrl);
+        console.log(LOG_SCOPE, "failed to load license remotely from", remoteUrl);
         sendError(spdxId);
     });
 }
@@ -69,7 +71,7 @@ function loadRemote(spdxId, localUrl, remoteUrl) {
 WorkerScript.onMessage = function(message) {
     if (message.spdxId === undefined || message.spdxId === "") {
         error = true;
-        console.error("cannot load license without spdx id");
+        console.error(LOG_SCOPE, "cannot load license without spdx id");
         sendError("");
         return;
     }
@@ -78,7 +80,7 @@ WorkerScript.onMessage = function(message) {
         try {
             var o = JSON.parse(xhr.responseText);
             if (!o || typeof o !== "object") throw 1;
-            console.log("license loaded locally from", message.localUrl);
+            console.log(LOG_SCOPE, "license loaded locally from", message.localUrl);
             sendSuccess(message.spdxId, o['name'], o['licenseText']);
         }
         catch (e) {
