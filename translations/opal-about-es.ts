1b2413488a6930e64d8e3fc570201a1214abd623
Kamborio <Kamborio15@users.noreply.hosted.weblate.org>
2025-11-24 10:02:41 +0100

    Translated using Weblate (Spanish)
    Currently translated at 98.5% (66 of 67 strings)
    
    Co-authored-by: Kamborio <Kamborio15@users.noreply.hosted.weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/opal/opal-about/es/
    Translation: Opal for SailfishOS/Module: Opal.About

--- translations/opal-about-es.ts
+++ translations/opal-about-es.ts
@@ -39,7 +39,7 @@
         <location filename="../Opal/About/private/ContributorsAttributionRepeater.qml" line="68"/>
         <location filename="../Opal/About/private/LicenseListPart.qml" line="68"/>
         <source>Homepage</source>
-        <translation type="unfinished">Inicio</translation>
+        <translation>Página de inicio</translation>
     </message>
     <message>
         <location filename="../Opal/About/AboutPageBase.qml" line="814"/>
