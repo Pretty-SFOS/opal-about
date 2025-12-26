1b69cc39c56b6386dbcc2a714ad013c3d80f8ab8
Priit Jõerüüt <jrthwlate@users.noreply.hosted.weblate.org>
2025-12-26 10:00:30 +0100

    Translated using Weblate (Estonian)
    Currently translated at 100.0% (68 of 68 strings)
    
    Co-authored-by: Priit Jõerüüt <jrthwlate@users.noreply.hosted.weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/opal/opal-about/et/
    Translation: Opal for SailfishOS/Module: Opal.About

--- translations/opal-about-et.ts
+++ translations/opal-about-et.ts
@@ -112,7 +112,7 @@
     <message>
         <location filename="../Opal/About/private/LicensePage.qml" line="80"/>
         <source>You are using a mobile data connection.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sa kasutad mobiilset andmesideühendust.</translation>
     </message>
     <message numerus="yes">
         <location filename="../Opal/About/private/LicensePage.qml" line="93"/>
